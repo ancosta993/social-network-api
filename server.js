00449652e24f175a7ec92c5880aad7a8d6c718d1
Anthony Dcosta <adcosta2000@gmail.com>
2022-07-25 22:40:05 -0400

    added mongoose and express connections

---
+++ server.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const express = require('express');
+
+const app = express();
+PORT = process.env.PORT || 3001;
+
+// express middlewears
+app.use(express.json()); // to parse json data
+app.use(express.urlencoded({extended: true}));
+
+// turn on all the routes
+app.use(require('./routes'));
+
+// establish mongoose connection
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/social-networ-api', {
+   useNewUrlparser: true,
+   useUnifiedTopology:true
+});
+
+// log mongo queries
+mongoose.set('debug', true);
+
+// start the server
+app.listen(PORT, () => {
+   console.log(`Connected to ${PORT}`)
+})
